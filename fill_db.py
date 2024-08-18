02ef1c3ec70d2e23b53002552dd4b012e44840e6
whereissam <sam135642@gmail.com>
2024-08-18 15:26:50 +0800

    feat: change embedding db method & system prompt

--- fill_db.py
+++ fill_db.py
@@ -16,7 +16,7 @@ collection = chroma_client.get_or_create_collection(name="Crypto_World")
 
 # loader = PyPDFDirectoryLoader(DATA_PATH)
 loaders = [
-    PyPDFDirectoryLoader(DATA_PATH),  # First PDF directory
+    # PyPDFDirectoryLoader(DATA_PATH),  # First PDF directory
     TextLoader("PayFi.txt")          # Text files loader (or any other type of loader)
 ]
 # Load all documents from multiple sources
@@ -24,24 +24,26 @@ raw_documents = []
 for loader in loaders:
     raw_documents.extend(loader.load())  # Combine the loaded documents
 
-# Test for embedded data
+# ----- Test for embedded data ---------
 print(len(raw_documents))
 page = raw_documents[len(raw_documents) -1]
-print(page.page_content[0:2000])
+print(page.page_content[0:500])
+print("\n\n---------------------\n\n")
+# ----- Test for embedded data ---------
 
 # splitting the document
 
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=300,
-    chunk_overlap=100,
+    chunk_size=600,
+    chunk_overlap=150,
     length_function=len,
     is_separator_regex=False,
+    separators=["\n\n", "\n", ". ", " ", ""],
 )
 
 chunks = text_splitter.split_documents(raw_documents)
 
-# # preparing to be added in chromadb
-
+# preparing to be added in chromadb
 documents = []
 metadata = []
 ids = []
@@ -52,37 +54,37 @@ for chunk in chunks:
     documents.append(chunk.page_content)
     ids.append("ID"+str(i))
     metadata.append(chunk.metadata)
-
     i += 1
 
 # adding to chromadb
-
-
 collection.upsert(
     documents=documents,
     metadatas=metadata,
     ids=ids
 )
 
-# Test for data already embedded
+# ----- Test for data already embedded ---------
 
 # Verify the number of documents in the collection
 num_documents = collection.count()
 print(f"Number of documents in the collection: {num_documents}")
-
+print("\n\n---------------------\n\n")
 # Retrieve and print a specific document by ID
 retrieved_document = collection.get(ids=["ID0"])
 print(f"Retrieved document: {retrieved_document}")
-
+print("\n\n---------------------\n\n")
 # List all IDs in the collection
 all_ids = collection.get(ids=[])
 print(f"All document IDs: {all_ids['ids']}")
-
+print("\n\n---------------------\n\n")
 # Query the collection for documents containing the word "vegetables"
 results = collection.query(
-    query_texts=["payfi"],
-    n_results=5
+    query_texts=["shefi"],
+    n_results=2
 )
 
-for result in results['documents']:
-    print(result)
+print(results)
+# for result in results['documents']:
+#     print(result)
+
+# ----- Test for data already embedded ---------
